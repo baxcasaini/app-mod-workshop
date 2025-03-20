0a8f722757336c10d1fc071f88d5d2149364257e
baxcasaini <baxcasaini@gmail.com>
2025-03-20 16:12:35 +0100

    Update config.php wrong pwd

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db_host = getenv('DB_HOST') ?: '34.31.66.153';
 $db_name = getenv('DB_NAME') ?: 'image_catalog';
 $db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
-$db_pass = getenv('DB_PASS') ?: 'CLOUDSQL_INSTANCE_PASSWORD';
+$db_pass = getenv('DB_PASS') ?: 'wrongpwd';
 // Note getenv() is PHP 5.3 compatible
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
