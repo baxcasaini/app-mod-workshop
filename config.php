d24e038f8fabb8d80d454543dae00d57a47c7ac4
baxcasaini <baxcasaini@gmail.com>
2025-03-20 15:32:57 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-// Configurazione del database
-$db_host = '34.31.66.153';
-$db_name = 'image_catalog';
-$db_user = 'appmod-phpapp-user';
-$db_pass = 'CLOUDSQL_INSTANCE_PASSWORD';
-
+// Database configuration with ENV variables. Set default values as well 
+$db_host = getenv('DB_HOST') ?: '34.31.66.153';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'CLOUDSQL_INSTANCE_PASSWORD';
+// Note getenv() is PHP 5.3 compatible
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
